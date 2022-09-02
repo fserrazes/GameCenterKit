db4a95a83bdd0932dfd709d93f7ad4cc7c463991
Flavio Serrazes <flavio.serrazes@icloud.com>
2022-09-02 12:30:35 +0100

    Set the minimum iOS target to 13.0

--- Package.swift
+++ Package.swift
@@ -5,22 +5,15 @@ import PackageDescription
 
 let package = Package(
     name: "GameCenterKit",
-    products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "GameCenterKit",
-            targets: ["GameCenterKit"]),
+    platforms: [
+        .macOS(.v10_15),
+        .iOS(.v13),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+    products: [
+        .library(name: "GameCenterKit", targets: ["GameCenterKit"]),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "GameCenterKit",
-            dependencies: []),
+        .target(name: "GameCenterKit", dependencies: []),
         .testTarget(
             name: "GameCenterKitTests",
             dependencies: ["GameCenterKit"]),
